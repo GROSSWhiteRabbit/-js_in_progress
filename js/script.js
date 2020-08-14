60720f8c66be816534baf0ef7efcd5243217971b
GrossWhiteRabbit <vowa.kononenko@gmail.com>
2020-08-14 19:23:25 +0300

    'food=disc'

--- js/script.js
+++ js/script.js
@@ -158,15 +158,111 @@ window.addEventListener('DOMContentLoaded', ()=>{
   }
   listenCloseModal(modal);
 
-  const modalTimerId = setTimeout(showModal, 15000, modal);
+//   const modalTimerId = setTimeout(showModal, 15000, modal);
   
   function showModalByScroll (){
-    if(document.documentElement.scrollTop +  document.documentElement.clientHeight === document.documentElement.scrollHeight) {
-        showModal(modal);
+    if(window.pageYOffset +  document.documentElement.clientHeight === document.documentElement.scrollHeight) {
         window.removeEventListener('scroll', showModalByScroll);
+  
+        showModal(modal);
+        
+        
     }
+    
   }
 
 
   window.addEventListener('scroll', showModalByScroll);
 });
+
+//MenuItem
+
+class MenuCard {
+    constructor(img, alt, tittle, desc, price, parentSelector, ...clases){
+        this.img = img;
+        this.alt = alt;
+        this.tittle = tittle;
+        this.desc = desc;
+        this.price = price;
+        this.parent = document.querySelector(parentSelector);
+        this.transfer = 27;
+        this.changeToUAH();
+        this.clases = clases;
+    }
+    render(){
+        const element = document.createElement('div');
+        if(this.clases.length === 0 ) {
+            this.element = 'menu__item';
+            element.classList.add(this.element);
+            
+        } else {
+            element.classList.add( ...this.clases);
+        }
+        
+        element.innerHTML = `
+        <img src=${this.img} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">Меню "${this.tittle}"</h3>
+        <div class="menu__item-descr">${this.desc}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>` ;   
+        this.parent.append(element);
+    }
+    changeToUAH(){
+        return this.price *= this.transfer;
+    }
+}
+
+
+new MenuCard(
+    'img/tabs/vegy.jpg',
+    'vegy',
+    'Фитнес',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container',
+    
+).render();
+
+new MenuCard(
+    'img/tabs/elite.jpg',
+    'elite',
+    'Премиум',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14,
+    '.menu .container',
+    
+).render();
+
+new MenuCard(
+    'img/tabs/post.jpg',
+    'post',
+    'Постное',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    21,
+    '.menu .container' 
+).render();
+
+
+// Forms
+
+const forms = document.querySelectorAll('form');
+
+function postData(form) {
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        const formData = new FormData(form),
+        request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'multipart/form-data');
+        request.send()
+    });
+}
+    
+
+
+
+
+ 
